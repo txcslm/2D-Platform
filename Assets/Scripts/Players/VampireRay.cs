2d71d2f0fb2b423519fdf3afd20cc19dedc6324e
OuroborosTFB <hmmm.true@gmail.com>
2024-02-17 21:34:29 +0300

    1

--- Assets/Scripts/Players/VampireRay.cs
+++ Assets/Scripts/Players/VampireRay.cs
@@ -1,50 +1,80 @@
 using EnemyMovement;
 using Players.PlayerMovement;
+using System.Collections;
 using UnityEngine;
 
 public class VampireRay : MonoBehaviour
 {
-	[SerializeField] private float _rayDistance = 10f;
-	[SerializeField] private PlayerStats _playerStats;
-	[SerializeField] private Player _player;
-	[SerializeField] private LayerMask _enemyLayerMask;
+    [SerializeField] private float _rayDistance = 10f;
+    [SerializeField] private PlayerStats _playerStats;
+    [SerializeField] private Player _player;
+    [SerializeField] private LayerMask _enemyLayerMask;
+    [SerializeField] private float _vampirismDuration = 6f;
 
-	private void Update()
-	{
-		if (Input.GetKeyDown(KeyCode.E))
-			CastVampireRay();
-	}
+    private bool _isVampirismActive = false;
+    private float _vampirismStartTime;
 
-	private void CastVampireRay()
-	{
-		CastRay(transform.right);
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && !_isVampirismActive)
+        {
+            StartCoroutine(CastVampireRay());
+        }
 
-		if (_player.GetComponent<SpriteRenderer>().flipX)
-			CastRay(-transform.right);
-	}
+        if (_isVampirismActive)
+        {
+            UpdateVampirism();
+        }
+    }
 
-	private void CastRay(Vector2 direction)
-	{
-		Vector2 rayOrigin = transform.position;
+    private void UpdateVampirism()
+    {
+        if (Time.fixedTime - _vampirismStartTime >= _vampirismDuration)
+        {
+            _isVampirismActive = false;
+        }
+        else
+        {
+            CastRay(transform.right);
 
-		RaycastHit2D hit = Physics2D.Raycast(rayOrigin, direction, _rayDistance, _enemyLayerMask);
+            if (_player.GetComponent<SpriteRenderer>().flipX)
+                CastRay(-transform.right);
+        }
+    }
 
-		Debug.DrawRay(rayOrigin, direction * _rayDistance, Color.red, 1f);
+    private IEnumerator CastVampireRay()
+    {
+        _isVampirismActive = true;
+        _vampirismStartTime = Time.fixedTime;
 
-		DamageAndHeal(hit);
-	}
+        while (_isVampirismActive)
+        {
+            yield return new WaitForFixedUpdate();
+        }
+    }
 
-	private void DamageAndHeal(RaycastHit2D hit)
-	{
-		if (hit.collider != null)
-		{
-			if (hit.collider.TryGetComponent(out Enemy enemy))
-			{
-				enemy.TakeDamage(_player.Damage);
+    private void CastRay(Vector2 direction)
+    {
+        Vector2 rayOrigin = transform.position;
 
-				if (_playerStats != null)
-					_playerStats.Heal(_player.Damage);
-			}
-		}
-	}
+        RaycastHit2D hit = Physics2D.Raycast(rayOrigin, direction, _rayDistance, _enemyLayerMask);
+
+        Debug.DrawRay(rayOrigin, direction * _rayDistance, Color.red, 1f);
+
+        DamageAndHeal(hit);
+    }
+
+    private void DamageAndHeal(RaycastHit2D hit)
+    {
+        if (hit.collider != null)
+        {
+            if (hit.collider.TryGetComponent(out Enemy enemy))
+            {
+                enemy.TakeDamage(_player.Damage);
+
+                if (_playerStats != null)
+                    _playerStats.Heal(_player.Damage);
+            }
+        }
+    }
 }
